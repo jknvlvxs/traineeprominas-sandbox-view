9f834e249941148741b48a52a0fea2d4ea6d907d
jknvlvxs <julio619alves@gmail.com>
2019-06-17 14:32:00 -0300

    create user view to get all users

--- src/app/user/users/users.component.ts
+++ src/app/user/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { User } from 'src/model/user';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  displayedColumns: string[] = [ 'id', 'name', 'lastName', 'profile'];
+  dataSource: User[];
+  isLoadingResults = true;
 
-  constructor() { }
+  constructor(private _api: ApiService) { }
 
   ngOnInit() {
+    this._api.getUsers()
+    .subscribe(res => {
+      this.dataSource = res;
+      console.log(this.dataSource);
+      this.isLoadingResults = false;
+    }, err => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
   }
 
 }
