14ef8cef7a074f3da1df83f07dac0b29fc819f5f
jknvlvxs <julio619alves@gmail.com>
2019-06-17 16:19:05 -0300

    create get all for course

--- src/app/course/courses/courses.component.ts
+++ src/app/course/courses/courses.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { Course } from 'src/model/course';
 
 @Component({
   selector: 'app-courses',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
+  displayedColumns: string[] = [ 'id', 'name', 'period', 'city', 'teacher', 'action'];
+  dataSource: Course[];
+  isLoadingResults = true;
 
-  constructor() { }
+  constructor(private _api: ApiService) { }
 
   ngOnInit() {
+    this._api.getCourses()
+    .subscribe(res => {
+      this.dataSource = res;
+      console.log(this.dataSource);
+      this.isLoadingResults = false;
+    }, err => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
   }
 
 }
