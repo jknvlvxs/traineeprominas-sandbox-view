368bfcbba671050f2b91c0a45fd120044e7b4658
jknvlvxs <julio619alves@gmail.com>
2019-06-17 17:57:44 -0300

    tries to get filtered user(error) and then update and delete

--- src/app/user/user-filtered/user-filtered.component.ts
+++ src/app/user/user-filtered/user-filtered.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/service/api.service';
+import { User } from 'src/model/user';
 
 @Component({
   selector: 'app-user-filtered',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-filtered.component.css']
 })
 export class UserFilteredComponent implements OnInit {
-
-  constructor() { }
+  user: User = { id: 0, name: '', lastName: '', profile: ''};
+  isLoadingResults = true;
+  constructor(private router: Router, private route: ActivatedRoute, private api: ApiService) { }
 
   ngOnInit() {
+    this.getUser(this.route.snapshot.params.id);
   }
 
+  getUser(id) {
+    this.api.getUser(id)
+      .subscribe(data => {
+        this.user = data;
+        this.isLoadingResults = false;
+      });
+  }
+
+  deleteProduto(id) {
+    this.isLoadingResults = true;
+    this.api.deleteUser(id)
+      .subscribe(res => {
+          this.isLoadingResults = false;
+          this.router.navigate(['/user']);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
+  }
 }
--- src/app/user/user-update/user-update.component.ts
+++ src/app/user/user-update/user-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ApiService } from 'src/service/api.service';
 
 @Component({
   selector: 'app-user-update',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-update.component.css']
 })
 export class UserUpdateComponent implements OnInit {
-
-  constructor() { }
+  id = 0;
+  userForm: FormGroup;
+  name = '';
+  lastName = '';
+  profile = '';
+  isLoadingResults = false;
+  constructor(private router: Router, private route: ActivatedRoute, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-  }
+    this.getUser(this.route.snapshot.params.id);
+    this.userForm = this.formBuilder.group({
+   name : [null, Validators.required],
+   lastName : [null, Validators.required],
+   profile : [null, Validators.required]
+ });
+ }
 
+ getUser(id) {
+  this.api.getUser(id).subscribe(data => {
+    this.id = data.id;
+    this.userForm.setValue({
+      name: data.name,
+      lastName: data.lastName,
+      profile: data.profile
+    });
+  });
+}
+
+putUser(form: NgForm) {
+  this.isLoadingResults = true;
+  this.api.putUser(this.id, form)
+    .subscribe(res => {
+        this.isLoadingResults = false;
+        this.router.navigate(['/user'/* + this.id*/]);
+      }, (err) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      }
+    );
+}
 }
