fa63d7eeeaa232db40106eec3f4d3a21e67d2f24
jknvlvxs <julio619alves@gmail.com>
2019-06-17 17:57:01 -0300

    create user post

--- src/app/user/user-create/user-create.component.ts
+++ src/app/user/user-create/user-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ApiService } from 'src/service/api.service';
 
 @Component({
   selector: 'app-user-create',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-create.component.css']
 })
 export class UserCreateComponent implements OnInit {
-
-  constructor() { }
+  userForm: FormGroup;
+  name = '';
+  lastName = '';
+  profile = '';
+  isLoadingResults = false;
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+     this.userForm = this.formBuilder.group({
+    name : [null, Validators.required],
+    lastName : [null, Validators.required],
+    profile : [null, Validators.required]
+  });
   }
 
+  postUser(form: NgForm) {
+    this.isLoadingResults = true;
+    this.api.postUser(form)
+      .subscribe(res => {
+          // const id = res['id'];
+          this.isLoadingResults = false;
+          this.router.navigate(['/user'/*, id*/]);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        });
+  }
 }
