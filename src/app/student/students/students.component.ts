f761ec20d8953d3dcd3831550ebf3d29f8474128
jknvlvxs <julio619alves@gmail.com>
2019-06-17 16:18:28 -0300

    create get all for student

--- src/app/student/students/students.component.ts
+++ src/app/student/students/students.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { Student } from 'src/model/student';
 
 @Component({
   selector: 'app-students',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
+  displayedColumns: string[] = [ 'id', 'name', 'lastName', 'age', 'course', 'action'];
+  dataSource: Student[];
+  isLoadingResults = true;
 
-  constructor() { }
+  constructor(private _api: ApiService) { }
 
   ngOnInit() {
+    this._api.getStudents()
+    .subscribe(res => {
+      this.dataSource = res;
+      console.log(this.dataSource);
+      this.isLoadingResults = false;
+    }, err => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
   }
 
 }
