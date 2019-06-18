3f1d89ab06a32c75853695d35aa888ef85fc3744
jknvlvxs <julio619alves@gmail.com>
2019-06-18 16:27:25 -0300

    finishes get filtered student view

--- src/app/student/student-filtered/student-filtered.component.ts
+++ src/app/student/student-filtered/student-filtered.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/service/api.service';
+import { Student } from 'src/model/student';
 
 @Component({
   selector: 'app-student-filtered',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-filtered.component.css']
 })
 export class StudentFilteredComponent implements OnInit {
-
-  constructor() { }
+  student: Student = { id: 0, name: '', lastName: '', age: 0, course: []};
+  isLoadingResults = true;
+  constructor(private router: Router, private route: ActivatedRoute, private api: ApiService) { }
 
   ngOnInit() {
+    this.getStudent(this.route.snapshot.params.id);
   }
 
+  getStudent(id) {
+    this.api.getStudent(id)
+      .subscribe(data => {
+        this.isLoadingResults = false;
+        if (data) {
+        this.student = data[0];
+        }
+      });
+  }
+
+  deleteStudent(id) {
+    this.isLoadingResults = true;
+    this.api.deleteStudent(id)
+      .subscribe(res => {
+          this.isLoadingResults = false;
+          this.router.navigate(['/student']);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
+  }
 }
