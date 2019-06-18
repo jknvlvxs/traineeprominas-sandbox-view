3f9c6561676e90ad6301a4659802afe46164219a
jknvlvxs <julio619alves@gmail.com>
2019-06-18 09:34:15 -0300

    create view to post teacher

--- src/app/teacher/teacher-create/teacher-create.component.ts
+++ src/app/teacher/teacher-create/teacher-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ApiService } from 'src/service/api.service';
 
 @Component({
   selector: 'app-teacher-create',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teacher-create.component.css']
 })
 export class TeacherCreateComponent implements OnInit {
-
-  constructor() { }
+  teacherForm: FormGroup;
+  name = '';
+  lastName = '';
+  phd = false;
+  isLoadingResults = false;
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+     this.teacherForm = this.formBuilder.group({
+    name : [null, Validators.required],
+    lastName : [null, Validators.required],
+    phd : [null, Validators.required]
+  });
   }
 
+  postTeacher(form: NgForm) {
+    this.isLoadingResults = true;
+    this.api.postTeacher(form)
+      .subscribe(res => {
+          // const id = res['id'];
+          this.isLoadingResults = false;
+          this.router.navigate(['/teacher'/*, id*/]);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        });
+  }
 }
