4a6b9bf96526ec1c2fa30c0a8152081cacfc8096
jknvlvxs <julio619alves@gmail.com>
2019-06-18 16:26:19 -0300

    finishes get filtered course view

--- src/app/course/course-filtered/course-filtered.component.ts
+++ src/app/course/course-filtered/course-filtered.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/service/api.service';
+import { Course } from 'src/model/course';
 
 @Component({
   selector: 'app-course-filtered',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-filtered.component.css']
 })
 export class CourseFilteredComponent implements OnInit {
-
-  constructor() { }
+  course: Course = { id: 0, name: '', period: 0, city: '', teacher: []};
+  isLoadingResults = true;
+  constructor(private router: Router, private route: ActivatedRoute, private api: ApiService) { }
 
   ngOnInit() {
+    this.getCourse(this.route.snapshot.params.id);
   }
 
+  getCourse(id) {
+    this.api.getCourse(id)
+      .subscribe(data => {
+        this.isLoadingResults = false;
+        if (data) {
+        this.course = data[0];
+        }
+      });
+  }
+
+  deleteCourse(id) {
+    this.isLoadingResults = true;
+    this.api.deleteCourse(id)
+      .subscribe(res => {
+          this.isLoadingResults = false;
+          this.router.navigate(['/course']);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
+  }
 }
