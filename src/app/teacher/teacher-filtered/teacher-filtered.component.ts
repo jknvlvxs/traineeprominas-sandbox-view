cb5e4f1d2f6e1d420c585d389cc9289a1b435f7c
jknvlvxs <julio619alves@gmail.com>
2019-06-18 09:33:47 -0300

    create teacher view to get filtered

--- src/app/teacher/teacher-filtered/teacher-filtered.component.ts
+++ src/app/teacher/teacher-filtered/teacher-filtered.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/service/api.service';
+import { Teacher } from 'src/model/teacher';
 
 @Component({
   selector: 'app-teacher-filtered',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teacher-filtered.component.css']
 })
 export class TeacherFilteredComponent implements OnInit {
-
-  constructor() { }
+  teacher: Teacher = { id: 0, name: '', lastName: '', phd: false};
+  isLoadingResults = true;
+  constructor(private router: Router, private route: ActivatedRoute, private api: ApiService) { }
 
   ngOnInit() {
+    this.getTeacher(this.route.snapshot.params.id);
   }
 
+  getTeacher(id) {
+    this.api.getTeacher(id)
+      .subscribe(data => {
+        this.isLoadingResults = false;
+        if (data) {
+          this.teacher = data[0];
+        }
+      });
+  }
+
+  deleteTeacher(id) {
+    this.isLoadingResults = true;
+    this.api.deleteTeacher(id)
+      .subscribe(res => {
+          this.isLoadingResults = false;
+          this.router.navigate(['/teacher']);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
+  }
 }
