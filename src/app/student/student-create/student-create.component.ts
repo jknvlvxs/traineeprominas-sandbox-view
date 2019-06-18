ec888675930714795c3ca20bc3bb2dd0c8f158f3
jknvlvxs <julio619alves@gmail.com>
2019-06-18 16:27:41 -0300

    finishes post student view

--- src/app/student/student-create/student-create.component.ts
+++ src/app/student/student-create/student-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ApiService } from 'src/service/api.service';
+import { Course } from 'src/model/course';
 
 @Component({
   selector: 'app-student-create',
@@ -6,10 +10,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-create.component.css']
 })
 export class StudentCreateComponent implements OnInit {
+  dataSource: Course[];
+  studentForm: FormGroup;
+  name = '';
+  lastName = '';
+  age = 0;
+  course: Course[];
+  isLoadingResults = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) {
   }
 
+  ngOnInit() {
+    this.getCourse();
+    this.studentForm = this.formBuilder.group({
+      name : [null, Validators.required],
+      lastName : [null, Validators.required],
+      age : [null, Validators.required],
+      course : [[]],
+  });
+}
+
+  postStudent(form: NgForm) {
+    this.isLoadingResults = true;
+    this.api.postStudent(form)
+      .subscribe(res => {
+          // const id = res['id'];
+          this.isLoadingResults = false;
+          this.router.navigate(['/student'/*, id*/]);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        });
+  }
+
+  getCourse() {
+    this.api.getCourses()
+    .subscribe(res => {
+      this.dataSource = res;
+      this.course = this.dataSource.map((item: Course) => {
+        const course = new Course();
+        course.id = item.id;
+        course.name = item.name;
+        course.period = item.period;
+        course.city = item.city;
+        course.teacher = item.teacher;
+        return course;
+      });
+    }, err => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
+  }
 }
